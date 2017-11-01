55aa460833bffa560b57b459135c7f2f3a8c2069
UBUNTU <ubuntu@ubuntu-pc>
2017-11-02 02:51:21 +1100

    add more unit test for requirements

--- src/components/home/home.controller.spec.js
+++ src/components/home/home.controller.spec.js
@@ -41,4 +41,44 @@ describe('Component: Home', () => {
     promise.resolve({data:{}});
     expect(scope).toBeDefined();
   });
+
+  it('should onClick to be define', () => {
+    promise.resolve({data:{}});
+    expect(scope.onClick).toBeDefined();
+  });
+
+  it('should have same scope as config service response', () => {
+    promise.resolve({data:{"buttons":[16,10,-43,-34],"bars":[85,33,76],"limit":210}})
+    promise.then(() => {
+      expect(scope.buttons).toEqual([16,10,-43,-34]);
+      expect(scope.bars).toEqual([85,33,76]);
+      expect(scope.limit).toEqual(210);
+    })
+  });
+
+  it('should change progressbar value, On Click', () => {
+    promise.resolve({data:{"buttons":[16,10,-43,-34],"bars":[85,33,76],"limit":210}})
+    promise.then(() => {
+      scope.onClick(0,10);
+      expect(scope.bars[0]).toEqual(95);
+    })
+  });
+
+  it('should go over limit', () => {
+    promise.resolve({data:{"buttons":[16,10,-43,-34],"bars":[85,33,76],"limit":210}})
+    promise.then(() => {
+      scope.onClick(0,800);
+      expect(scope.bars[0]).toEqual(885);
+    })
+  });
+
+  it('should not go under zero', () => {
+    promise.resolve({data:{"buttons":[16,10,-43,-34],"bars":[85,33,76],"limit":210}})
+    promise.then(() => {
+      scope.onClick(0,-800);
+      expect(scope.bars[0]).toEqual(0);
+      scope.onClick(0,10);
+      expect(scope.bars[0]).toEqual(10);
+    })
+  });
 });
